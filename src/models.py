ea713c8dbfce5001859785007f41598894a597be
M.V <marco.mov98@gmail.com>
2025-01-22 12:53:42 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Identification (Base):
+    __tablename__ = 'Identification'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Characters (Base):
+    __tablename__ = 'Characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    birth_year = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    identification_id = Column(Integer, ForeignKey('identification.id'))
+    identification = relationship(Identification)
+
+class Planets (Base):
+    __tablename__ = 'Planets'
+    id = Column(Integer, primary_key=True)
+    terrain = Column(String(250), nullable=False)
+    surface_wate = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    rotation_period= Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    identification_id = Column(Integer, ForeignKey('identification.id'))
+    identification = relationship(Identification)
+
+class Starships(Base):
+    __tablename__ = 'Starships'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    length = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    identification_id = Column(Integer, ForeignKey('identification.id'))
+    identification = relationship(Identification)
+
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    id = Column(Integer, primary_key=True)
+    identification_id = Column(Integer, ForeignKey('identification.id'))
+    identification = relationship(Identification)
+
 
     def to_dict(self):
         return {}
